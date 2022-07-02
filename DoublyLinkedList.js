89ff9edd4f7b5db6d0009fecc0c1325c571a30ac
ivvlko <ivankoevbg@gmail.com>
2022-07-02 13:36:24 +0300

    classes skeletons

---
+++ DoublyLinkedList.js
@@ -0,0 +1,16 @@
+class Node{
+    constructor(value, next, previous){
+        this.value = value;
+        this.next = next;
+        this.previous = previous;
+    }
+}
+
+
+class DoublyLinkeList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
