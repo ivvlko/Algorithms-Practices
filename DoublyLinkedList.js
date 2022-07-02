9f1a1647929f9a560ae8eb8f526d12ab3f5be7e3
ivvlko <ivankoevbg@gmail.com>
2022-07-02 17:27:09 +0300

    push print and print reverse methods

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,8 +1,8 @@
 class Node{
-    constructor(value, next, previous){
+    constructor(value){
         this.value = value;
-        this.next = next;
-        this.previous = previous;
+        this.next = null;
+        this.previous = null;
     }
 }
 
@@ -13,4 +13,49 @@ class DoublyLinkeList{
         this.tail = null;
         this.length = 0;
     }
+
+    push(element){
+        let newNode = new Node(element);
+        if (!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.previous = this.tail;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length ++;
+        return this;
+    }
+
+    print(){
+        let current = this.head;
+        let res = [];
+        while (current){
+            res.push(current.value);
+            current = current.next;
+        }
+        return res;
+    }
+
+    printReverse(){
+        let current = this.tail;
+        let res = [];
+
+        while (current){
+            res.push(current.value);
+            current = current.previous;
+        }
+        return res;
+    }
 }
+
+let arr = new DoublyLinkeList();
+
+arr.push(1);
+arr.push(2);
+arr.push(3);
+arr.push(42);
+
+console.log(arr.print());
+console.log(arr.printReverse());
