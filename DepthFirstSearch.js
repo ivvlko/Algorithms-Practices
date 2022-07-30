addf04835f188b495d3dc8f46610626a52e4c264
ivvlko <ivankoevbg@gmail.com>
2022-07-30 10:56:53 +0300

    pretify code

--- DepthFirstSearch.js
+++ DepthFirstSearch.js
@@ -2,19 +2,9 @@ const {bst, node} = require("./BinarySearchTree.js");
 
 let tree = new bst();
 tree.root = new node(40);
-tree.add(55);
-tree.add(45);
-tree.add(38);
-tree.add(39);
-tree.add(1);
-tree.add(21);
-tree.add(72);
-tree.add(65);
-tree.add(99);
-tree.add(42);
-tree.add(19);
-tree.add(24);
 
+let elementsToAdd = [40, 55, 45, 38, 39, 1, 21, 72, 65, 99, 42, 19, 24]
+elementsToAdd.map((x) => tree.add(x));
 
 //https://visualgo.net/en/bst > use this nice web app to visualize your tree
 
