f0137fdc218e11f23fe3ea9f873b1d261ba68b76
ivvlko <ivankoevbg@gmail.com>
2022-08-03 17:59:01 +0300

    extractMax

--- BinaryHeap.js
+++ BinaryHeap.js
@@ -18,6 +18,24 @@ class MaxBinaryHeap{
         return this.store;
     }
 
+    extractMax(){
+        this.store.unshift();
+        this.store[0] = this.store.pop();
+        let current = 0;
+        let child1 = current * 2 + 1;
+        let child2 = current * 2 + 2;   
+        
+        while (this.store[current] < this.store[child1] || this.store[current] < this.store[child2] ){
+            let bigger = this.store[child1] > this.store[child2] ? child1 : child2;
+            [this.store[current], this.store[bigger]] = [this.store[bigger], this.store[current]];
+            current = bigger;
+            child1 = current * 2 + 1;
+            child2 = current * 2 + 2;   
+        }
+
+        return this.store;
+    }
+
     print(){
         return this.store;
     }
@@ -28,5 +46,6 @@ mbh = new MaxBinaryHeap();
 let valuesToPut = [50, 49, 29,  30, 35, 46, 55, 75]
 
 valuesToPut.map(x => mbh.insert(x));
-
+console.log(mbh.print());
+mbh.extractMax();
 console.log(mbh.print());
